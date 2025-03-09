d994e2027f3b719ed1a1bc19dd16e31d613e4226
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-03-08 19:01:31 -0600

    Add OLL Cross algorithms with triggers.

--- app/src/main/java/com/example/cubical/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/cubical/algorithm/Algorithm.kt
@@ -1,8 +1,8 @@
 package com.example.cubical.algorithm
 
 data class Algorithm (
-    val type: String,
-    val group: String,
-    val name: String,
-    val moves: String
+    val type:   String,
+    val group:  String,
+    val name:   String,
+    val moves:  String
 )
--- app/src/main/java/com/example/cubical/algorithm/AlgorithmData.kt
+++ app/src/main/java/com/example/cubical/algorithm/AlgorithmData.kt
@@ -2,7 +2,12 @@ package com.example.cubical.algorithm
 
 object AlgorithmData {
     val ollAlgorithms = listOf(
-        Algorithm("OLL", "Cross", "OCLL6 - 26", "R U2' R' U' R U' R'"),
-        Algorithm("OLL", "Cross", "OCLL7 - 27", "R U R' U R U2' R'")
+        Algorithm("OLL", "Cross", "OCLL6 - 26", "(R U2' R' U') (R U' R')"),
+        Algorithm("OLL", "Cross", "OCLL7 - 27", "(R U R' U) (R U2' R')"),
+        Algorithm("OLL", "Cross", "OCLL1 - 21", "(R U2' R' U') (R U R' U') (R U' R')"),
+        Algorithm("OLL", "Cross", "OCLL2 - 22", "R U2' R2' U' R2 U' R2' U2' R"),
+        Algorithm("OLL", "Cross", "OCLL4 - 24", "(r U R' U') (r' F R F')"),
+        Algorithm("OLL", "Cross", "OCLL5 - 25", "F' (r U R' U') r' F R"),
+        Algorithm("OLL", "Cross", "OCLL3 - 23", "R2 D' R U2' R' D R U2' R")
     )
 }
