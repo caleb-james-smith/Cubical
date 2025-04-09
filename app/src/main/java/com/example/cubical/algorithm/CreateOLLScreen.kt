edfba4fd1c4021d018f9d980e75006661b1981a9
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-04-08 22:19:10 -0500

    Simplify material3 import statements.

--- app/src/main/java/com/example/cubical/algorithm/CreateOLLScreen.kt
+++ app/src/main/java/com/example/cubical/algorithm/CreateOLLScreen.kt
@@ -3,10 +3,7 @@ package com.example.cubical.algorithm
 import android.content.Context
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.cubical.loadAlgorithmsFromJson
