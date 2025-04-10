86ad792c3c3aa9beee25bdbf20a7181a7caf54f1
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-04-10 18:23:28 -0500

    Add total number of OLL algorithms to top of screen. Spacing needs to be adjusted.

--- app/src/main/java/com/example/cubical/algorithm/CreateOLLScreen.kt
+++ app/src/main/java/com/example/cubical/algorithm/CreateOLLScreen.kt
@@ -1,13 +1,17 @@
 package com.example.cubical.algorithm
 
 import android.content.Context
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.text.style.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.cubical.loadAlgorithmsFromJson
 import com.example.cubical.R
@@ -33,12 +37,29 @@ fun CreateOLLScreen(context: Context, navController: NavController) {
             )
         }
     ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier.padding(paddingValues)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+                .padding(top = paddingValues.calculateTopPadding())
         ) {
-            groupedAlgorithms.forEach { (groupName, algorithms) ->
-                item {
-                    AlgorithmGroup(groupName = groupName, algorithms = algorithms)
+            Text(
+                text = "Total Algorithms: ${algorithms.size}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Left
+            )
+
+            LazyColumn(
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                groupedAlgorithms.forEach { (groupName, algorithms) ->
+                    item {
+                        AlgorithmGroup(groupName = groupName, algorithms = algorithms)
+                    }
                 }
             }
         }
