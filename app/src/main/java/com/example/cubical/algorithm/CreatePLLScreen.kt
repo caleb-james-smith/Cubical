924f57c5639184966177b5d23ac68c324f872d7c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-04-10 22:26:05 -0500

    Consolidate PLL imports; now consistent with OLL imports.

--- app/src/main/java/com/example/cubical/algorithm/CreatePLLScreen.kt
+++ app/src/main/java/com/example/cubical/algorithm/CreatePLLScreen.kt
@@ -1,20 +1,15 @@
 package com.example.cubical.algorithm
 
 import android.content.Context
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.*
+import androidx.compose.ui.text.style.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
